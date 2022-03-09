442f414ca616b500fccb3cd3bfb3036a5315eb0e
Amrita <amrita.chanda@gmail.com>
2022-03-09 12:10:27 +0530

    WIP: Building page generation framework by fetching data from Sanity

--- .eleventy.js
+++ .eleventy.js
@@ -16,6 +16,27 @@ module.exports = function (eleventyConfig) {
     //cacheFile: "", // don't cache results to a file
   });
 
+  let markdownIt = require('markdown-it');
+	let markdownItAnchor = require('markdown-it-anchor');
+	let markdownIt_options = {
+		html: true, //  Enable HTML tags in source
+		// breaks: true, // newlines in paragraphs are rendered as <br>
+		linkify: true, // Autoconvert URL-like text to links
+		typographer: true, // (c) will show up as copyright symbol, etc.
+	};
+	let anchor_opts = {
+		permalink: true,
+		permalinkClass: 'direct-link',
+		permalinkSymbol: '#',
+	};
+
+	eleventyConfig.setLibrary('md', markdownIt(markdownIt_options).use(markdownItAnchor, anchor_opts));
+
+	eleventyConfig.addFilter('markdownify', function (value) {
+		const md = new markdownIt(markdownIt_options);
+		return md.render(value);
+	});
+
   return {
     dir: {
       input: "src",
